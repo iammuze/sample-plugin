9acd770dbcc9bb0124f19a829618221d7ab16af7
vikasiwp <108919165+vikasiwp@users.noreply.github.com>
2023-01-31 15:38:02 +0530

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@
  * @version       1.0.4
  *
  * @wordpress-plugin
- * Plugin Name:   InstaWP New Name
+ * Plugin Name:   InstaWP Sample Plugin
  * Plugin URI:    https://instawp.com
  * Description:   InstaWP Git Deployment testing
  * Version:       1.0.10
