0d2d95eb981d2236ca2b0479dd483052e660311d
Vikas Singhal <vikas.programmer@gmail.com>
2023-01-31 16:34:43 +0530

    12 version

--- plugin.php
+++ plugin.php
@@ -10,7 +10,7 @@
  * Plugin Name:   InstaWP Sample Plugin
  * Plugin URI:    https://instawp.com
  * Description:   InstaWP Git Deployment testing
- * Version:       1.0.11
+ * Version:       1.0.12
  * Author:        Vikas
  * Author URI:    https://instawp.com
  * Text Domain:   instawp-sample-plugin
